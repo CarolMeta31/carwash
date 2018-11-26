b9f03dfa9233022e44f96b0d3b0426eed583ef6e
caro <nxaba31@gmail.com>
2018-11-26 15:12:51 +0200

    edit profile ts

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the EditProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, Loading, Alert} from 'ionic-angular';
+import 'firebase/database';
+import 'firebase/auth';
+
+import firebase, { User } from 'firebase/app';
+
 
 @IonicPage()
 @Component({
@@ -14,6 +13,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'edit-profile.html',
 })
 export class EditProfilePage {
+  private loading: any
+  username:string='';
+  email:string='';
+  phone:string;
+
+  imgPreview = 'https://firebasestorage.googleapis.com/v0/b/carwashapp-4fa12.appspot.com/o/logo.png?alt=media&token=4008c01c-9eaf-43c0-a26e-0f605f6b6627';
+
+  moveon=true;
+
+ userProfile:firebase.database.Reference;
+  picture: string = null;
+ public base64Image : string;
+ isOn:boolean;
+status:string;
+currentUser:User
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
