84e810c50d5c7e8275fcdfa3125e86983607bd38
Lwazi Tongo <jambilwah@gmail.com>
2019-01-17 11:21:41 +0200

    restructuring of the app design using slides instead of cars

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,5 @@
+import { OperationalDetailsProvider } from './../../providers/operational-details/operational-details';
+import { MainDetailsProvider } from './../../providers/main-details/main-details';
 import { MainDetailsPage } from './../main-details/main-details';
 import { CarPage } from './../car/car';
 import { CarwashProvider } from './../../providers/carwash/carwash';
@@ -5,7 +7,7 @@ import { EntertainmentPage } from './../entertainment/entertainment';
 import { CarDetailsPage } from './../car-details/car-details';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Time } from '@angular/common';
 import {FormControl} from '@angular/forms';
 
 @Component({
@@ -15,15 +17,99 @@ import {FormControl} from '@angular/forms';
 export class HomePage {
 
   Cards:string='';
+ 
 
+
+  //carwash details declaration
+  testRadioOpen: boolean;
+  testRadioResult;
+  entertainmentAvailable:boolean;
+  entertainmentArea:string;
+   numberOfCarsSedan:number;
+  numberOfCarsSuv:number;
+  numberOfCarsVan:number;
+  numberOfCarsMini:number;
+  numberOfCarsTruck:number;
+  maleEmployees:number;
+  femaleEmployees:number;
+  //slide3 declaractions
+  cars=[];
+  car='suv';
+  suvDuration:number;
+  sedanDuration:number;
+  miniDuration:number;
+  vanDuration:number;
+  truckDuration:number;
+
+  suvCost:string;
+  sedanCost:string;
+  miniCost:string;
+  vanCost:string;
+  truckCost:string;
  
   
 
 
-  constructor(public navCtrl: NavController,private carwashPro:CarwashProvider) {
+  constructor(public navCtrl: NavController,private carsPro:CarwashProvider,
+    private carwashPro:CarwashProvider ,private operationsPro:OperationalDetailsProvider,private mainPro:MainDetailsProvider) {
 
   }
+  //slide 1 main details
+  createMainDetails(
+    carwashName:string,
+    location:string,
+    openHours:Time,
+    closinghours:Time,
+    
+    satrdayHrsOpen:Time,
+    satrdayHrsclose:Time,
+    sundayHrsOpen:Time,
+    sundayHrsClose:Time
+  ): void {
+    this.mainPro
+      .createCarwashMainDetails(carwashName,
+        location,
+        openHours,
+        closinghours,
 
+        satrdayHrsOpen,
+        satrdayHrsclose,
+        sundayHrsOpen,
+        sundayHrsClose)
+      .then(newCarwashDetails => {
+        //goes back to welcome page
+        this.navCtrl.pop();
+      });
+  }
+  //slide 2 car details
+  saveCarOperationsDetails(
+    entertainmentAvail:boolean,
+    entertainmentArea:string,
+     numberOfCarsSedan:number,
+    numberOfCarsSuv:number,
+    numberOfCarsVan:number,
+    numberOfCarsMini:number,
+    numberOfCarsTruck:number,
+    maleEmployees:number,
+    femaleEmployees:number,
+  
+  ): void {
+    this.operationsPro
+      .createCarOperationsDetails(entertainmentAvail,
+        entertainmentArea,
+        numberOfCarsSedan,
+        numberOfCarsSuv,
+        numberOfCarsVan,
+        numberOfCarsMini,
+        numberOfCarsTruck,
+        maleEmployees,
+        femaleEmployees)
+      .then(newCarwashOperations => {
+        this.navCtrl.pop();
+      });
+    }
+   
+//slide 3 
   nextMainDetails(){
     this.navCtrl.push(MainDetailsPage);
   }
@@ -38,7 +124,55 @@ export class HomePage {
     this.navCtrl.push(CarPage);
      }
 
-
+     createCarsDetail(
+      suvDuration:number,
+      sedanDuration:number,
+      miniDuration:number,
+      vanDuration:number,
+      truckDuration:number,
+  
+      suvCost:string,
+      sedanCost:string,
+       miniCost:string,
+       vanCost:string,
+       truckCost:string,
+      
+  ): void {
+    this.carsPro
+      .createCarsDetails(suvDuration,
+        sedanDuration,
+        miniDuration,
+        vanDuration,
+        truckDuration,
+    
+        suvCost,
+        sedanCost,
+         miniCost,
+         vanCost,
+         truckCost,)
+      .then(newEvent => {
+        this.navCtrl.pop();
+      });
+    }
+  
+    nextsed(){
+      this.car='sedan';
+    }
+  
+    nextmin(){
+      this.car='mini';
+    }
+  
+    nextsuv(){
+      this.car='suv'
+    }
+  
+    nextvan(){
+      this.car='van'
+    }
+    nexttruck(){
+      this.car='truck'
+    }
 
  
     
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,3 +1,7 @@
+import { CarwashListDetailsPage } from './../carwash-list-details/carwash-list-details';
+import { OperationalDetailsProvider } from './../../providers/operational-details/operational-details';
+import { CarwashProvider } from './../../providers/carwash/carwash';
+import { MainDetailsProvider } from './../../providers/main-details/main-details';
 import { LoginPage } from './../login/login';
 import { AuthProvider } from './../../providers/auth/auth';
 import { ListDataPage } from './../list-data/list-data';
@@ -13,24 +17,114 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'welcome.html',
 })
 export class WelcomePage {
+//declaring list variables
+  public carwashList: Array<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private authPro:AuthProvider) {
+  key:string;
+  public carwashoperations: Array<any>;
+
+  keys:string;
+ public carsDetails: Array<any>;
+
+  constructor(public navCtrl: NavController,private carPro:CarwashProvider,
+    private mainPro:MainDetailsProvider,private operationsPro:OperationalDetailsProvider,
+     public navParams: NavParams,private authPro:AuthProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad WelcomePage');
-  }
+    //Now we need to get that list of events from Firebase
+    this.mainPro.getEventList().on("value", carwashListSnapshot => {
+      this.carwashList= [];
+      carwashListSnapshot.forEach(snap => {
+        this.carwashList.push({
+          id: snap.key,
+          carwashName: snap.val().carwashName,
+          carwashLocation: snap.val().carwashLocation,
+          weekdayOpen: snap.val().weekdayOpen,
+          weekdayClose: snap.val().weekdayClose,
+          
+          saturdayOpen: snap.val().saturdayOpen,
+          saturdayClose:snap.val().saturdayClose,
+          sundayOpen:snap.val().sundayOpen,
+          sundayClose:snap.val().sundayClose,
+        });
+        return false;
+      });
+    });
+console.log('cars',this.carwashList);
+//Now we need to get that list of events from Firebase
+this.operationsPro.getoperationsList().on("value", carwashListSnapshot => {
+  this.carwashoperations= [];
+  carwashListSnapshot.forEach(snap => {
+    this.carwashoperations.push({
+      id: snap.key,
+      entertainAvailable: snap.val(). entertainAvailable,
+      typeOfArea: snap.val().typeOfArea,
+      sedanCars: snap.val().sedanCars,
+      suvCars: snap.val().suvCars,
+      
+      vanCars: snap.val().vanCars,
+      miniCars:snap.val(). miniCars,
+      truckCars:snap.val().truckCars,
+      males:snap.val().males,
+      females:snap.val().females,
+    });
+    return false;
+  });
+});
+ //Now we need to get that list of events from Firebase
+ this.carPro.getcarsList().on("value", carwashListSnapshot => {
+  this.carsDetails= [];
+  carwashListSnapshot.forEach(snap => {
+    this.carsDetails.push({
+      id: snap.key,
+      durationForSUV: snap.val().durationForSUV,
+      durationForSedan: snap.val().durationForSedan,
+      durationForMini: snap.val().durationForMini,
+      durationForVan: snap.val().durationForVan,
+      durationForTruck: snap.val().durationForTruck,
+
+       costOfSuv: snap.val().costOfSuv,
+       costOfSedan:snap.val().costOfSedan,
+       costOfMini:snap.val().costOfMini,
+       costOfVan:snap.val().costOfVan,
+       costOfTruck:snap.val().costOfTruck
+    });
+    return false;
+  });
+});
+console.log('operations',this.carwashoperations);
+}
+
   goToCreate(): void {
     this.navCtrl.push(HomePage);
   }
   
-  goToList(): void {
-    this.navCtrl.push(ListDataPage);
-  }
-
-  logOut(): void {
+    logOut(): void {
     this.authPro.logoutUser().then(() => {
       this.navCtrl.setRoot(LoginPage);
     });
   }
+  // function to send users to the event detail
+goToCarwashDetail(carwashId: string):void {
+  //calls the other two functions
+   this.checkoutOps();
+   this.checkoutCarsDetails();
+ 
+   this.navCtrl.push(CarwashListDetailsPage, { carwashId:carwashId, keyId:this.key , keysID:this.keys});
+ }
+ //function search the other key from the next category
+ checkoutOps():void {
+   for(var i=0;i<this.carwashoperations.length;i++){
+     this.key=this.carwashoperations[i].id;
+   }
+   
+ }
+ //function search key from the third category
+ checkoutCarsDetails():void {
+   for(var i=0;i<this.carsDetails.length;i++){
+     this.keys=this.carsDetails[i].id;
+   }
+   
+ }
 }
